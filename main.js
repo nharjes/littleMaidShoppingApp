be808d5a17a6830f54c51e2d3854170b04e7a567
nharjes <132439937+nharjes@users.noreply.github.com>
2025-10-01 17:14:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import {
   getDatabase,
   ref,
-  push,
+  push, 
   onValue, 
   remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
